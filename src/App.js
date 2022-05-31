066cbdee46dc41a69dc6f0b7bc9b7260c960f304
Anton Meshchaninov <tonym7@yandex.ru>
2022-05-31 17:46:02 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<Header />
+			<div className="App">
+				Hello
+			</div>
+			<Footer />
+		</div>
+	);
+};
+
+const Header = () => {
+	return (
+		<header className="header">Header</header>
+	);
+};
+
+const Footer = () => {
+	return (
+		<footer className="footer">Footer</footer>
+	);
+};
+
+
 
 export default App;
